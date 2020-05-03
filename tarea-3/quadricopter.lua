ee95383b9991bdc43f75ed8cf32c524fa0ac5e91
Angel Maldonado <angelgesus@gmail.com>
2020-05-03 19:51:28 +0200

    Implement visual servoing to the quadcopter

--- tarea-3/quadricopter.lua
+++ tarea-3/quadricopter.lua
@@ -6,7 +6,7 @@
     end
 
     -- Detatch the manipulation sphere:
-    targetObj=sim.getObjectHandle('testpoint0')
+    targetObj=sim.getObjectHandle('20cmHighPillar10cm')
     --sim.setObjectParent(targetObj,-1,true)
 
     -- This control algo was quickly written and is dirty and not optimal. It just serves as a SIMPLE example
@@ -53,6 +53,18 @@
     frontView=sim.floatingViewAdd(0.7,0.9,0.2,0.2,0)
     sim.adjustView(floorView,floorCam,64)
     sim.adjustView(frontView,frontCam,64)
+    
+    
+     camera=sim.getObjectHandle('Vision_sensor')
+     res=sim.getVisionSensorResolution(camera)
+     
+     
+     
+     lider=sim.getObjectHandle('Khepera_IV')
+     
+     targetHigh = 1
+     imgPos = {0.5,0.5}
+         corrCam = {0,0}
 end
 
 
@@ -64,45 +76,98 @@ end
 
 
 function sysCall_actuation() 
-    s=sim.getObjectSizeFactor(d)
     
-    pos=sim.getObjectPosition(d,-1)
-    if (fakeShadow) then
-        itemData={pos[1],pos[2],0.002,0,0,1,0.2*s}
-        sim.addDrawingObjectItem(shadowCont,itemData)
+    
+	
+	liderDetection = false	
+	result,t0,t1=sim.readVisionSensor(camera) -- Here we read the image processing camera!
+    if t1 then -- in t1 we should have the blob information if the camera was set-up correctly
+            
+        blobCount=t1[1]
+        dataSizePerBlob=t1[2]
+        lowestYofDetection=100
+            
+        -- Now we go through all blobs:
+        for i=1,blobCount,1 do
+			idxb = 2+(i-1)*dataSizePerBlob
+			blobSize		  = t1[idxb + 1]
+            blobOrientation   = t1[idxb + 2]
+            blobPos			  = { t1[idxb + 3], t1[idxb + 4] }
+            blobBoxDimensions = { t1[idxb + 5], t1[idxb + 6] }
+    
+			if blobSize>0.0001 then
+				blobCol = sim.getVisionSensorImage(camera,res[1]*blobPos[1],res[2]*blobPos[2],1,1)
+				if blobCol[1]>blobCol[2] and blobCol[1]>blobCol[3] then
+					--print('X: ', blobPos[1], 'Y: ', blobPos[2], 'Size:', blobSize, 'Orient:', blobOrientation )
+					imgPos = blobPos
+					liderDetection = true
+				end
+			end
+		end
+	end
+    
+
+    s = sim.getObjectSizeFactor( d )
+    pos = sim.getObjectPosition( d, -1 )
+    if fakeShadow then
+        itemData={ pos[1], pos[2], 0.002, 0, 0, 1, 0.2*s }
+        sim.addDrawingObjectItem( shadowCont, itemData )
     end
     
-    -- Vertical control:
+    
     targetPos = sim.getObjectPosition(targetObj,-1)
-    targetPos[3] = targetPos[3] + 0.2
     pos = sim.getObjectPosition(d,-1)
+    orit = sim.getObjectOrientation(d,-1)
+    
+
+    -- Rising quadcopter if drone is no detected
+    high = liderDetection and 0.4 or 4
+        
+    alpha = 0.99
+    targetHigh = high*(1 - alpha) + alpha*targetHigh
+    
+    targetPos[3] = targetHigh
+    
+    -- Vertical control:
+
     l = sim.getVelocity(heli)
     e = targetPos[3] - pos[3]
     cumul = cumul + e
     thrust = 5.335 + pParam*e + iParam*cumul + dParam*(e-lastE) + vParam*l[3]
     lastE = e
     
+    
+    --Corregir proyeccion cámara
+    if liderDetection then
+		corrCam[1] = -1*math.sin(orit[2])
+		corrCam[2] =  1*math.sin(orit[1])
+    end
+    
+    
     -- Horizontal control: 
     sp = sim.getObjectPosition( targetObj, d)
     m  = sim.getObjectMatrix( d, -1)
     
+    sp[1] = 0.5 - imgPos[1]  + corrCam[1]
+    sp[2] = 0.5 - imgPos[2]  + corrCam[2]
+    
     
     vy = sim.multiplyVector( m, {0,1,0} )
     alphaE = vy[3] - m[12]
-    alphaCorr = 0.25*alphaE + 2.1*(alphaE - pAlphaE)
+    alphaCorr = 0.15*alphaE + 1.1*(alphaE - pAlphaE)
     pAlphaE = alphaE
-    alphaCorr = alphaCorr + sp[2]*0.005 + 1*(sp[2] - psp2)
+    alphaCorr = alphaCorr + sp[2]*0.01 + 0.5*(sp[2] - psp2)
     psp2 = sp[2]
     
     vx = sim.multiplyVector( m, {1,0,0} )
     betaE = vx[3] - m[12]
-    betaCorr = -0.25*betaE - 2.1*(betaE - pBetaE)
+    betaCorr = -0.15*betaE - 1.1*(betaE - pBetaE)
     pBetaE = betaE
-    betaCorr = betaCorr - sp[1]*0.005 - 1*(sp[1] - psp1)
+    betaCorr = betaCorr - sp[1]*0.01 - 0.5*(sp[1] - psp1)
     psp1 = sp[1]
     
     -- Rotational control:
-    euler     = sim.getObjectOrientation(d,targetObj)
+    euler     = sim.getObjectOrientation( d, -1) 
     rotCorr   = euler[3]*0.1+2*(euler[3]-prevEuler)
     prevEuler = euler[3]
     
@@ -116,5 +181,16 @@ function sysCall_actuation()
     for i=1,4,1 do
         sim.setScriptSimulationParameter(propellerScripts[i],'particleVelocity',particlesTargetVelocities[i])
     end
+    
+    
+    
+
+    
+
+    
+
+    
+    
+    
 end 
 
