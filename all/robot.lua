922c74d10b01e9707e7a4224106c7e27cd9d0470
Angel Maldonado <angelgesus@gmail.com>
2020-06-06 11:27:31 +0200

    Refactor some functios

--- all/robot.lua
+++ all/robot.lua
@@ -12,8 +12,8 @@ module(..., package.seeall )
 	-- Braintenberg Algorithm param
 	local SdistMax = 0.25 --m  
 	
-	local Ws = {{ 0,  -1.5, 0.9, 1.3,   1 }, 
-			{   1, 1.3, 0.9,  -1.5, 0 } }
+	local Ws = {{ -0.7,  -0.7, 0., 0.8,   1 }, 
+			{   1, 0.8, 0.5,  -0.7, -0.7 } }
 			
 	
 	function robot:new( class, id, theta, dist )
@@ -30,12 +30,14 @@ module(..., package.seeall )
 		for i=1,5 do 
 			self.sensor[i] = sim.getObjectHandle( 'K4_Infrared_' .. i + 1 .. id )
 		end
+		self.enDetection = true;
 		self.theta = theta
 		self.dist  = dist
 		self.wl_sma = lib.sma(15)
 		self.wr_sma = lib.sma(15)
 		self.p_robotOrig = simGetObjectPosition( self.body , -1 )
-		self.d_toTarg = 0
+		self.d_toTarget = 0
+		self.ori_err = 0
 		self.slaveErr = 0
 		self.Kf = 0.0
 
@@ -45,14 +47,14 @@ module(..., package.seeall )
 		self.Vmin	= 0.12
 		self.Kr     = 0.2
 		self.Ki		= 0.1
-		self.wmax   = 2
+		self.wmax   = 1.5
 
 		if class == 'master' then
 			self.Vmax   = 0.19
 			self.Vmin	= 0.08
 			self.Kr     = 0.3
 			self.Ki		= 0.1
-			self.wmax   = 0.7	
+			self.wmax   = 0.9	
 		end
 		
 		return o
@@ -105,14 +107,14 @@ module(..., package.seeall )
 		self.controlRule = "basic"
 	end
 
-	function robot:runBasicControlRule ( d_toTarget , ori_err )
+	function robot:runBasicControlRule ( )
 	
 		Kp = 0.22
 		wmax = 1.5
 		
-		v={}
-		v[1] = Kp * d_toTarget
-		v[2] = wmax * math.sin( ori_err )
+		local v={}
+		v[1] = Kp * self.d_toTarget
+		v[2] = wmax * math.sin( self.ori_err )
 		
 		return v
 	end
@@ -124,21 +126,27 @@ module(..., package.seeall )
 		self.Kr     = Kr
 		self.Ki		= Ki
 		self.wmax   = Wmax
-		
 	end
 
-	function robot:runAdvanceControlRule ( d_toTarget, d_toOrig, ori_err )
-
+	function robot:runAdvanceControlRule ( )
+		
 		local v={}
+		p_robot  = sim.getObjectPosition( self.body, -1 )
+		if self.class == 'master' then
+			d_toOrig = math.sqrt( ( p_robot[1] - self.p_robotOrig[1] )^2 + ( p_robot[2] - self.p_robotOrig[2] )^2 )
+		else 
+			d_toOrig = 0.2
+		end
+
 		if d_toOrig < self.Ki then
 			v[1] = math.max ( d_toOrig * (self.Vmax / self.Ki), self.Vmin )
-		elseif d_toTarget < Kr  then
-			v[1] = d_toTarget * (self.Vmax / self.Kr)
+		elseif self.d_toTarget < self.Kr  then
+			v[1] = self.d_toTarget * (self.Vmax / self.Kr)
 		else
-				v[1] = self.Vmax
+			v[1] = self.Vmax
 		end
 			
-		v[2] = self.wmax * math.sin( ori_err )
+		v[2] = self.wmax * math.sin( self.ori_err )
 		
 		return v
 	end	
@@ -157,7 +165,10 @@ module(..., package.seeall )
 		return lib.MatMul( Ws, dist )
 	end
 
-	
+	function robot:disableDetection( )
+		self.enDetection = false;
+	end
+
 	function robot:isAnyDetection( )
 		local detection = false
 		for i=1,5 do
@@ -187,45 +198,37 @@ module(..., package.seeall )
 	
 	function robot:getAngularSpeed( p_target )
 			
+		-- Execute a control rule.
+		self.d_toTarget = self:getDistanceToTarget ( p_target )
+		self.ori_err = self:getOrientationErrorToTarget ( p_target )
 		
-			p_robot  = sim.getObjectPosition( self.body, -1 )
-			
-			if self.class == 'master' then
-				d_toOrig = math.sqrt( ( p_robot[1] - self.p_robotOrig[1] )^2 + ( p_robot[2] - self.p_robotOrig[2] )^2 )
-			else 
-				d_toOrig = 0.2
-			end
-	
-			self.d_toTarg = self:getDistanceToTarget ( p_target )
-			ori_err = self:getOrientationErrorToTarget ( p_target )
-			
-			local v={}
-			if  self.controlRule == "advance" then
-				v = self:runAdvanceControlRule ( self.d_toTarg, d_toOrig, ori_err )
-			elseif self.controlRule == "basic" then
-				v = self:runBasicControlRule ( self.d_toTarg , ori_err )
-			else
-				print('Error control rule' )
-			end
+		local v={}
+		if  self.controlRule == "advance" then
+			v = self:runAdvanceControlRule ( )
+		elseif self.controlRule == "basic" then
+			v = self:runBasicControlRule ( )
+		else
+			print('Error control rule' )
+		end
 
-			-- Cooperative rule
-			if self.class == 'master' and self.Kf > 0.0 then
-				v[1] = v[1]  -  self.Kf*self.slaveErr
-				v[1] = v[1] < 0.05 and 0.05 or v[1]
-			end
-			
-					
-			if self:isAnyDetection( ) then
-				Sdist = self:getDistanceVector( )
-				res = self:calculateObs( Sdist ) 
-				wr = res[1][1]*Coef
-				wl = res[2][1]*Coef
-			else
-				-- Cinematic model, get the angular velocity wheels from linear 
-				-- and angular robot velocity.
-				wr = (2*v[1] - v[2]*wheelsdist) / (2*rwheel)
-				wl = (2*v[1] + v[2]*wheelsdist) / (2*rwheel)
-			end
+		-- Cooperative rule, only if master and Cooperative factor > 0
+		if self.class == 'master' and self.Kf > 0.0 then
+			v[1] = v[1]  -  self.Kf*self.slaveErr
+			v[1] = v[1] < 0.05 and 0.05 or v[1]
+		end
+		
+		
+		if self:isAnyDetection( ) and self.enDetection == true then
+			-- Calcula the angular velocity wheels if there is any detection.
+			Sdist = self:getDistanceVector( )
+			res = self:calculateObs( Sdist ) 
+			wr = res[1][1]*Coef
+			wl = res[2][1]*Coef
+		else
+			-- Calculate the angular velocity wheels from linear angular robot velocity.
+			wr = (2*v[1] - v[2]*wheelsdist) / (2*rwheel)
+			wl = (2*v[1] + v[2]*wheelsdist) / (2*rwheel)
+		end
 			
 		return wr, wl
 	end
@@ -235,7 +238,7 @@ module(..., package.seeall )
 		v[1] = vlineal
 		v[2] = vangular
 			
-		if self:isAnyDetection( ) then
+		if self:isAnyDetection( ) and self.enDetection == true then
 			Sdist = self:getDistanceVector( )
 			res = self:calculateObs( Sdist ) 
 			wr = res[1][1]*Coef
@@ -262,7 +265,7 @@ module(..., package.seeall )
 	
 	function robot:isTargetReach( p_target )
 	
-		return 0.005 > self.d_toTarg and true or false 
+		return 0.005 > self.d_toTarget and true or false 
 			
 	end
 	
@@ -287,7 +290,11 @@ module(..., package.seeall )
 	end
 
 	function robot:getErrorToTarget( )
-		return self.d_toTarg
+		return self.d_toTarget
+	end
+
+	function robot:getErrorOrientation( )
+		return self.ori_err
 	end
 
 	
