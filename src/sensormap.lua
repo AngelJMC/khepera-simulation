1aac952c58aabf582905ee9310d51e561f0e8f5c
Angel Maldonado <angelgesus@gmail.com>
2020-06-07 17:17:40 +0200

    Refactor and code clean for the slam task.

--- src/sensormap.lua
+++ src/sensormap.lua
@@ -1,21 +1,16 @@
 local slam = require("slam")
 
 function sysCall_init() 
-    mapSensor=sim.getObjectHandle('mapSensor')
     
-    --mapSensor = sim.handleVisionSensor('mapSensor')
+    mapSensor=sim.getObjectHandle('mapSensor')
     floatingView=sim.floatingViewAdd(0.8,0.3,0.5,0.8,0)
     sim.adjustView(floatingView,mapSensor,64)
-    cnt=0
-    senseEveryXPass=1
 
     local mapDimensions = {{-2600,2600}, {-2600,2600}} --mm  { {Xmin,Xmax}, {Ymin,Ymax}}
     res = sim.getVisionSensorResolution(mapSensor)
-    
-    local numCells = res --mm
-    print(res )
-    local slam = slam:new( mapDimensions, numCells )
-        
+    slam = slam:new( mapDimensions, res )
+
+    --Init image    
     imgData = { }
     for i=1, res[1]*res[2]*3 do
         imgData[i] = 0.5
@@ -25,48 +20,30 @@ end
 
 function sysCall_sensing() 
     
-
-    --Number/string signalValue=sim.waitForSignal("masterSensor")
+    -- Get all sensor info 
     rname = {"master", "master#0", "master#1", "master#2"}
     local sensorData = {}
     for i=1,4 do
         local pack = sim.getStringSignal( rname[i] )
         if pack then
             sensorData[i] = sim.unpackTable( pack )
-            --print(rname[i], sensorData[i])
         end
     end
 
+    slam:updateGridMap( sensorData )
 
-    cnt=cnt+1
-    if (cnt>=senseEveryXPass) then
-        cnt=0
-            
-        
-        --Iterate over each Khepera
-        for i=1,4 do
-            if sensorData[i] then
-                slam:updateNeighborhoodCells( sensorData[i] )
-            end
-            --print( sensorData[i] ) 
-        end
-
-        gridData = slam:getGrid ( )
-        for i=1,res[1] do
-            for j=1,res[2] do
-            
+    -- Update image from gridmap data.
+    gridData = slam:getGrid ( )
+    for i=1,res[1] do
+        for j=1,res[2] do   
             idx = (i-1)*(3*res[2]) + (j-1)*3+1
             imgData[idx] = gridData[i][j]
-            --print(gridData[i][j])
-            end
         end
-        
-        --print(imgData)
-        sim.setVisionSensorImage(mapSensor,imgData)
-        sim.handleVisionSensor(mapSensor)
-
-        --
     end
+    sim.setVisionSensorImage(mapSensor,imgData)
+    sim.handleVisionSensor(mapSensor)
+
+
 end 
 
 function sysCall_cleanup() 
--- src/slam.lua
+++ src/slam.lua
@@ -2,7 +2,8 @@ module(..., package.seeall )
 	local lib = require("auxlib")
 	local self = { }
 	
-	
+	Kd = 2
+
 	function slam:new( mapDimensions, numCells )
 	
 		o = o or {}
@@ -10,13 +11,7 @@ module(..., package.seeall )
 		self.__index = self
 
 		self.mapSize = mapDimensions
-		--self.cellSize = cellSize
-
 		self.numCells = numCells
-		--self.numCells = {}
-		--self.numCells[1] = math.abs( self.mapSize[1][1] - self.mapSize[1][2]) / self.cellSize; --Num cell X
-		--self.numCells[2] = math.abs( self.mapSize[2][1] - self.mapSize[2][2]) / self.cellSize; --Num cell Y
-
 		self.testpoint = sim.getObjectHandle('Dummy')
 		
         self.grid = {}          -- create the matrix
@@ -25,33 +20,29 @@ module(..., package.seeall )
           for j=1,self.numCells[2] do
             self.grid[i][j] = 0.5
           end
-        end
+		end
+		
         print("Created matrix ith size:", self.numCells[1], "x", self.numCells[2], "MapSize", self.mapSize )
-
 		return o
 	end
 	
-    
-    
 
 	function slam:test()
 		print("Library SLAM OOP!!")
 	end
 	
 	
-	function slam:getposition( )
-		return sim.getObjectPosition( self.body, -1 )
-	end 
-
-	function slam:transformCoordinatesToGrid( self, pos )
+	function slam:transformCoordinatesToGrid( coord )
+		
 		
 		px = ( self.numCells[1] - 0 )/( self.mapSize[1][2] - self.mapSize[1][1]) 
-		i = pos[1]*px + self.numCells[1]/2
-		
+		ix = lib.round( coord[1]*px*1000 + self.numCells[1]/2 )
+					
 		py = ( self.numCells[2] - 0 )/( self.mapSize[2][2] - self.mapSize[2][1]) 
-		j = pos[2]*px + self.numCells[2]/2
+		iy = lib.round( coord[2]*py*1000 + self.numCells[2]/2 )
 
-		print( "pos:", pos, "Gridx", i, "Gridy", j)
+		--print( "pos:", coord, "Gridx", ix, "Gridy", iy)
+		return ix, iy
 	end
 
 
@@ -59,56 +50,31 @@ module(..., package.seeall )
 		return self.grid
 	end
 
-	function slam:updateNeighborhoodCells( sensorIDs )
 
-			local distance = {}
-			--Iterate over each sensor
-			for i=1,5 do
-				
-				local irDetect,irDist = sim.readProximitySensor( sensorIDs[i], sim_handle_all )
-				
-				sPos = sim.getObjectPosition( sensorIDs[i], -1 )
-				sOri = sim.getObjectOrientation( sensorIDs[i], -1 )
+	function slam:updateGridMap( sensorData )
 
-				dist = 0.25
-				if irDetect == 1 then
-					dist = irDist
-					--print(irDist)
-				end
+		local distance = {}
+		
+		--Iterate over each Khepera
+		for i=1,4 do
+			if sensorData[i] then
+				--Iterate over each sensor
+				sensorIDs = sensorData[i]
+				for i=1,5 do		
+					local irDetect,irDist = sim.readProximitySensor( sensorIDs[i], sim_handle_all )
 
-				sim.setObjectPosition( self.testpoint, sensorIDs[i] , {0,0,dist} )
-				
-				dPos = sim.getObjectPosition( self.testpoint, -1 )
-				coord = dPos
-				
-				--for i=1,4 do
-				i = 4
-					coord[1] = sPos[1] + (i/4)*(dPos[1] - sPos[1])
-					coord[2] = sPos[2] + (i/4)*(dPos[2] - sPos[2])
-					sim.setObjectPosition( self.testpoint,-1 , coord )
-
+					dist = ( irDetect == 1 ) and irDist or 0.25
+					sim.setObjectPosition( self.testpoint, sensorIDs[i] , {0,0,dist} )
+					coord = sim.getObjectPosition( self.testpoint, -1 )
 					--Transform coordenates to matrix grid
-					px = ( self.numCells[1] - 0 )/( self.mapSize[1][2] - self.mapSize[1][1]) 
-					ix = lib.round( coord[1]*px*1000 + self.numCells[1]/2 )
-					
-					py = ( self.numCells[2] - 0 )/( self.mapSize[2][2] - self.mapSize[2][1]) 
-					iy = lib.round( coord[2]*py*1000 + self.numCells[2]/2 )
-					
-					--print( "pos:", coord, "Gridx", lib.round(ix), "Gridy", lib.round(iy))
+					ix,iy = slam:transformCoordinatesToGrid( coord )
+					sensVal = ( irDetect == 1 ) and (0.5 + 0.5/Kd ) or 0.15
 
-					sensVal = 0
-					
-					if( irDetect == 1 ) then
-						sensVal = 0.8
-					else
-						sensVal = 0.1
-					end
-					
 					--Aplicar bayes
-					self.grid[iy][ix] = ( sensVal * self.grid[iy][ix] ) / ( sensVal * self.grid[iy][ix] + (1-sensVal)*(1-self.grid[iy][ix])) 
-					
-				--end
+					self.grid[iy][ix] = ( sensVal * self.grid[iy][ix] ) / ( sensVal * self.grid[iy][ix] + (1-sensVal)*(1-self.grid[iy][ix])) 	
+				end
 			end
+		end
 
 
 	end
